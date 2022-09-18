8019cb0a2af6e0b635ac407a57771c0909b74b69
Yusuke Kido <mail@yusuk.eki.do>
2022-09-18 10:53:55 +0900

    Proof of concept

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  const links = ['a&b'];
+</script>
+
+<h1>page 1</h1>
+
+{#each links as href}
+  <a {href}>{href}</a>
+{/each}
